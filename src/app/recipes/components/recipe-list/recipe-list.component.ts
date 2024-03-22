c46c84f01a92fe14dac805f5e1a27e6fa6000a6a
handyanaadesso <handyana-sumitra.atmaja@adesso.de>
2024-03-22 13:05:09 +0100

    added content to the recipes components

--- src/app/recipes/components/recipe-list/recipe-list.component.ts
+++ src/app/recipes/components/recipe-list/recipe-list.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../../models/recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.scss'
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', " This is a simply test", "https://www.eatingwell.com/thmb/LH-H61DAD-1Q3AgeN89BkrWKNEk=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Chicken-piccata-casserole-3x2-167-f44730f489cc4b9493547de1c76a3b93.jpg")
+  ];
 
+  constructor() {}
+
+  ngOnInit(): void { }
 }
